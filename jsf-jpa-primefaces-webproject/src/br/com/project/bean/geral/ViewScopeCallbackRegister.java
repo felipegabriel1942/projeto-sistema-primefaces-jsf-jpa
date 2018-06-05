0f8396d08dfe3c09288cb803b88bc2e9dbada0ca
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-06-05 12:36:00 -0300

    Criação de primeira classe para scopo de view

---
+++ jsf-jpa-primefaces-webproject/src/br/com/project/bean/geral/ViewScopeCallbackRegister.java
@@ -0,0 +1,21 @@
+package br.com.project.bean.geral;
+
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.ViewMapListener;
+
+public class ViewScopeCallbackRegister implements ViewMapListener{
+
+	@Override
+	public boolean isListenerForSource(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void processEvent(SystemEvent arg0) throws AbortProcessingException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
